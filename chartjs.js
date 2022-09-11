9b6c444038f1b4debf850f629dd193e5c86c5b6b
Marrocco-Simone <marrocco.simone00@gmail.com>
2022-09-11 19:48:28 +0200

    changed env port to PORT

--- chartjs.js
+++ chartjs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require("dotenv").config();
-const port = process.env.port;
+const port = process.env.PORT;
 
 const app = express();
 app.use(express.static(__dirname));
